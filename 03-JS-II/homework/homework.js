f44cf2f7eb13dc06dce50691b2e5d4ac46c7fac2
AndresHiguitaAMG <andreshiguitaamg@gmail.com>
2022-03-07 22:58:20 -0500

    redo bases as practice

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -135,7 +135,24 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if (numero % 15 === 0) {
+    return "fizzbuzz";
+  } else if (numero % 3 === 0) {
+    return "fizz";
+  } else if (numero % 5 === 0) {
+    return "buzz";
+  } else 
+  return numero;
 }
+
+// if (numero % 3 === 0 && numero % 5 === 0) {
+//   return "fizzbuzz";
+// } else if (numero % 3 === 0) {
+//   return "fizz";
+// } else if (numero % 5 === 0) {
+//   return "buzz";
+// } else 
+// return numero;
 
 function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
@@ -143,7 +160,22 @@ function operadoresLogicos(num1, num2, num3) {
   //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
-  //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  //Si no se cumplen ninguna de las condiciones anteriores, retornar false.
+  if (num1 === 0 || num2 === 0 || num3 === 0) {
+    return "Error"
+  }
+  //Algunos
+  if (num1 < 0 || num2 < 0 || num3 < 0) {
+    return "Hay negativos";
+  }
+  //Todos && los incluye 
+  if (num1 > num2 && num1 > num3 && num1 > 0) { 
+    return "Número 1 es mayor y positivo";
+  }
+  if (num3 > num1 || num3 > num2) {
+    return num3 = num3 + 1;
+  } 
+  return false;
 }
 
 function esPrimo(numero) {
@@ -152,29 +184,55 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero === 0 || numero === 1) return false;
+  for (var i = 2; i < numero; i++) {
+    if (numero % i === 0) return false;
+    return true;
+  }
 }
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
+  if (valor === true) {
+    return "Soy verdadero";
+  } else {
+    return "Soy falso";
+  }
+}
 
+function tablaDelSeis(){
+  //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
+  //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
+  //Escribe tu código aquí
+  var tablaDe6 = [];
+  for (let i = 0; i <= 10; i++){
+    tablaDe6.push(i * 6);
+  }
+  return tablaDe6;   
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  if (numero.length <= 3) {
+  var numeroentero = numero.toString()
+  if (numeroentero.length === 3){
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var i = 1;
+  do {
+    numero = numero + 5;
+    i++;
+  } while (i <= 8);
+  return numero;
 }
 
 
@@ -194,6 +252,7 @@ module.exports = {
   operadoresLogicos,
   esPrimo,
   esVerdadero,
+  tablaDelSeis,
   tieneTresDigitos,
   doWhile
 };
